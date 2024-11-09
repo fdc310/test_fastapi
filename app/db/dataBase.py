b00c8e8b3f410c3b39480d43c8176b519d4a7176
fdc310 <82008029+fdc310@users.noreply.github.com>
2024-11-09 17:56:15 +0800

    Update dataBase.py
    修改

--- app/db/dataBase.py
+++ app/db/dataBase.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine("mysql+pymysql://root:$2a$10$03NOBAUgnNH8nnTplWY2f.oWXn29wwDyd4RrrSy4X9aHwR1tNNx9m@47.115.205.23:3306/chat_vits", pool_size=15, max_overflow=30)
+engine = create_engine("mysql+pymysql://usaename:password@47.115.205.23:3306/chat_vits", pool_size=15, max_overflow=30)
 Base = declarative_base()
 metadata = Base.metadata
 metadata.bind = engine
